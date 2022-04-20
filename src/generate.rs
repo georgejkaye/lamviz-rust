5e7e44ec9a11bbad62026d4902f3345c73431573
George Kaye <georgejkaye@gmail.com>
2022-04-20 19:31:20 +0100

    Create generic term generating function

--- src/generate.rs
+++ src/generate.rs
@@ -1,57 +1,71 @@
+use itertools::Itertools;
 use lambda_calculus::Term;
 use lambda_calculus::{abs, app};
 
-pub fn generate_pure_terms(n: usize, k: usize) -> Vec<Term> {
-    match n {
-        0 => vec![],
-        1 => (1..(k + 1)).map(|x| Term::Var(x)).collect(),
-        n => {
-            let mut terms = vec![];
-            // The term inside an abstraction has one fewer subterm
-            // but one more free variable (the abstracted variable)
-            let abs_terms = generate_pure_terms(n - 1, k + 1);
-            for t in abs_terms {
-                terms.push(abs(t.clone()));
-            }
-            for i in 1..(n - 1) {
-                // Pick a number of subterms and free variables
-                let lhs = generate_pure_terms(i, k);
-                // The rhs has the complementary number of subterms, but the
-                // same number of free variables as these are pure terms
-                let rhs = generate_pure_terms(n - 1 - i, k);
-                // Each possible lhs is paired up with each possible rhs
-                for t1 in &lhs {
-                    for t2 in &rhs {
-                        terms.push(app(t1.clone(), t2.clone()));
-                    }
-                }
-            }
-            terms
+fn descending_list(n: usize) -> Vec<usize> {
+    let mut list = (1..n + 1).collect::<Vec<_>>();
+    list.reverse();
+    list
+}
+
+#[derive(Copy, Clone)]
+pub enum Fragment {
+    PURE,
+    LINEAR,
+    PLANAR,
+}
+
+fn generate_pure_contexts(ks: &Vec<usize>) -> Vec<(Vec<usize>, Vec<usize>)> {
+    vec![(ks.clone(), ks.clone())]
+}
+
+fn generate_planar_contexts(ks: &Vec<usize>) -> Vec<(Vec<usize>, Vec<usize>)> {
+    let mut contexts = vec![];
+    for j in 0..(ks.len() + 1) {
+        contexts.push((ks[..j].to_vec(), ks[j..].to_vec()));
+    }
+    contexts
+}
+
+fn generate_linear_contexts(ks: &Vec<usize>) -> Vec<(Vec<usize>, Vec<usize>)> {
+    let mut contexts = vec![];
+    let unique_perms = ks
+        .iter()
+        .permutations(ks.len())
+        .unique()
+        .collect::<Vec<_>>();
+    // println!("{:?}", unique_perms);
+    for j in 0..(ks.len() + 1) {
+        for ks in &unique_perms {
+            contexts.push((
+                ks[..j].iter().map(|i| **i).collect(),
+                ks[j..].iter().map(|i| **i).collect(),
+            ));
         }
     }
+    contexts
 }
 
-fn descending_list(x: usize, y: usize) -> Vec<usize> {
-    (x..y).collect()
-}
-
-pub fn generate_planar_terms(n: usize, k: usize) -> Vec<Term> {
-    fn generate_planar_terms_1(n: usize, ks: &mut Vec<usize>) -> Vec<Term> {
+pub fn generate_terms(n: usize, k: usize, fragment: Fragment) -> Vec<Term> {
+    fn generate_terms_helper(n: usize, ks: &mut Vec<usize>, fragment: Fragment) -> Vec<Term> {
         match n {
             0 => vec![],
             1 => match ks[..] {
-                [k] => vec![Term::Var(k)]
-                // If there are multiple free variables, there are no linear terms
-                // of size 1 as each variable must be used exactly once
-                _ => vec![]
+                [k] => vec![Term::Var(k)],
+                _ => match fragment {
+                    Fragment::PURE => ks.iter().map(|x| Term::Var(*x)).collect(),
+                    // If there are multiple free variables, there are no linear or planar
+                    // terms of size 1 as each variable must be used exactly once
+                    _ => vec![],
+                },
             },
             n => {
                 let mut terms = vec![];
-                // Under an abstraction, all the free variables are incremented by 1
+                // Under an abstraction, , y: usizeall the free variables are incremented by 1
                 ks.iter_mut().for_each(|k| *k += 1);
                 // Push the newest variable
                 ks.push(1);
-                let abs_terms = generate_planar_terms_1(n - 1, ks);
+                let abs_terms = generate_terms_helper(n - 1, ks, fragment);
                 for t in abs_terms {
                     terms.push(abs(t.clone()));
                 }
@@ -59,10 +73,18 @@ pub fn generate_planar_terms(n: usize, k: usize) -> Vec<Term> {
                 ks.iter_mut().for_each(|k| *k -= 1);
                 // We split the subterms between each side of the application
                 for i in 1..(n - 1) {
-                    // We also split up the context into two halves, preserving their order
-                    for j in 0..(ks.len() + 1) {
-                        let lhs = generate_planar_terms_1(i, &mut ks[..j].to_vec());
-                        let rhs = generate_planar_terms_1(n - 1 - i, &mut ks[j..].to_vec());
+                    // The two subterms may have different contexts
+                    let contexts = match fragment {
+                        Fragment::PURE => generate_pure_contexts(ks),
+                        Fragment::PLANAR => generate_planar_contexts(ks),
+                        Fragment::LINEAR => generate_linear_contexts(ks),
+                    };
+
+                    for (mut lhs_context, mut rhs_context) in contexts {
+                        let lhs = generate_terms_helper(i, &mut lhs_context, fragment);
+                        println!("{:?}", lhs);
+                        let rhs = generate_terms_helper(n - 1 - i, &mut rhs_context, fragment);
+                        println!("{:?}", rhs);
                         for t1 in &lhs {
                             for t2 in &rhs {
                                 terms.push(app(t1.clone(), t2.clone()));
@@ -77,8 +99,8 @@ pub fn generate_planar_terms(n: usize, k: usize) -> Vec<Term> {
     let mut context = {
         match k {
             0 => vec![],
-            k => descending_list(k - 1, 0),
+            k => descending_list(k),
         }
     };
-    generate_planar_terms_1(n, &mut context)
+    generate_terms_helper(n, &mut context, fragment)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 mod generate;
 
+use std::io;
+
 fn main() {
-    let terms = generate::generate_planar_terms(5, 0);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let n = input.trim().parse().unwrap();
+
+    let terms = generate::generate_terms(n, 3, generate::Fragment::LINEAR);
+    println!("There are {} terms", terms.len());
     for t in terms {
         println!("{}", t);
     }
